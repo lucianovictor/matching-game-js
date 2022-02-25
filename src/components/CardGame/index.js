81fadab4decba3e83af103305921868d447c4faa
lucianovictor <lucianocarvallho7@gmail.com>
2022-02-25 14:57:53 -0300

    :sparkles: Now is possible change the icon

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,9 +1,9 @@
 import './style.css';
 
-const CardGame = () =>  { 
+const CardGame = (icon = "alurapixel", alt = "logo da alura") =>  { 
   return `
     <article class="card-game">
-    <img src="images/alurapixel.png" alt="logo alura">
+    <img src="images/${icon}.png" alt="${alt} ">
     </article> 
   `;
 }
