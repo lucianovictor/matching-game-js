7693dd22917bc561aeb97e0c2f8e1fdd2c3537c9
lucianovictor <lucianocarvallho7@gmail.com>
2022-03-02 11:06:19 -0300

    :lipstick: solving transform and box-sizing

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -3,23 +3,22 @@ import CardGame from '../CardGame';
 import './style.css';
 
 const CardFrontBack = () => {
-  window.cardFrontBack = {}
+  window.cardFrontBack = {};
   window.cardFrontBack.handleClick = (event) => {
     const $origin = event.target;
-
     const $cardFrontBack = $origin.closest('.card-front-back');
 
-    $cardFrontBack.classList.toggle('active');
+    $cardFrontBack.classList.toggle('-active');
   }
-    
-  return`
-    <article class="card-front-back" onclick="cardFrontBack.handleClick(event)"> 
-    <div class="card -front" >
-      ${CardGame()}
-    </div> 
-    <div class="card -back">
-      ${CardGame("javascript", "logo fa JavaScript")}
-    </div>
+
+  return  `
+    <article class="card-front-back" onClick="cardFrontBack.handleClick(event)">
+      <div class="card -front">
+        ${CardGame()}
+      </div>
+      <div class="card -back">
+        ${CardGame("javascript", "Logo do JavaScript")}
+      </div>
     </article>
   `;
 }
